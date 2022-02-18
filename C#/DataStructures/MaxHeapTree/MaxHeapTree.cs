885d0c087e26b2ae193da46fe0a6e67d73eeac63
KlaivMrt <arc.aleister@gmail.com>
2022-02-19 01:29:41 +0200

    Max Heap Tree

---
+++ C#/DataStructures/MaxHeapTree/MaxHeapTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class MaxHeapTree
+    {
+
+        private int Left(int index)
+        {
+            return index * 2;
+        }
+
+        private int Right(int index)
+        {
+            return index * 2 + 1;
+        }
+
+        public void MaxHeapify(int[] array, int index)
+        {
+            int largest = index;
+            int left = Left(index);
+            int right = Right(index);
+
+            if (left <= array.Length && array[left - 1] > array[index - 1])
+                largest = left;
+
+            else
+                largest = index;
+
+            if (right <= array.Length && array[right - 1] > array[largest - 1])
+                largest = right;
+
+            if (largest != index)
+            {
+                int max = array[largest - 1];
+                int min = array[index - 1];
+
+                array[index - 1] = max;
+                array[largest - 1] = min;
+
+                Console.WriteLine(largest - 1);
+                MaxHeapify(array, largest);
+            }
+        }
+
+        public void BuildMaxHeap(int[] array)
+        {
+            int length = array.Length/2;
+            for (int i = length; i > 0; i--)
+                MaxHeapify(array, i);
+        }
+    }
+}
---
+++ C#/DataStructures/MaxHeapTree/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStructures
+{
+    
+
+    public class Program
+    {
+
+        public static int[] changes(int[] array)
+        {
+            int max = array[0];
+            int min = array[1];
+
+            array[1] = max;
+            array[0] = min;
+
+            return array;
+        }
+        public static void Main(string[] args)
+        {
+            int[] array = new int[] { 16, 4, 10, 14, 7, 9, 3, 2, 8, 1 };
+
+
+            MaxHeapTree tree = new MaxHeapTree();
+            tree.BuildMaxHeap(array);
+            //tree.MaxHeapify(array, 2);
+
+            foreach(int num in array)
+                Console.Write("{0} ", num);
+            //Console.WriteLine();
+            //foreach (int num in array)
+               // Console.Write("{0} ", num);
+        }
+    }
+}
