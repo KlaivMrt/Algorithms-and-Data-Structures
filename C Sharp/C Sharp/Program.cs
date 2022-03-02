7e8db59a3d207a839cb95196d9f3d5800ed34feb
KlaivMrt <arc.aleister@gmail.com>
2022-03-02 23:08:36 +0200

    -

--- C Sharp/C Sharp/Program.cs
+++ C Sharp/C Sharp/Program.cs
@@ -4,14 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            int[] array = new int[] { 10, 3, 1, 7, 8, 9, 2};
-            MinHeapTree minHeapTree = new MinHeapTree();
-
-            minHeapTree.BuildMinHeap(array);
-            minHeapTree.DisplayHeap();
-
-            minHeapTree.Delete(0);
-            minHeapTree.DisplayHeap();
+            
         }
     }
 }
